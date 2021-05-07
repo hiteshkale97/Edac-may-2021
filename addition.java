15d4d202b50872f2bb46c3db207712f5ca72a3ab
hiteshkale97 <83508463+hiteshkale97@users.noreply.github.com>
2021-05-07 19:19:58 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class addition
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter any two number");
+		float a= sc.nextFloat();
+		float b= sc.nextFloat();
+		float s= a+b;
+		System.out.println("sum of two number is "+s);
+	}
+}
